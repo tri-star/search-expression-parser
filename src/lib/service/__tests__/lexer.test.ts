f796b9a511d15ca1aef786f56d03608c73f59ce9
tristar <tristar.2nd@gmail.com>
2024-03-16 21:50:10 +0900

    スペースを含む場合のテストを追加

--- src/lib/service/__tests__/lexer.test.ts
+++ src/lib/service/__tests__/lexer.test.ts
@@ -14,6 +14,15 @@ describe('lexer', () => {
       ] satisfies Token[],
     },
     {
+      title: 'title = 123',
+      input: 'title = 123',
+      expected: [
+        { type: tokenTypes.TERM, value: 'title' },
+        { type: tokenTypes.EQUAL },
+        { type: tokenTypes.STRING, value: '123' },
+      ] satisfies Token[],
+    },
+    {
       title: '+',
       input: '1+2',
       expected: [
